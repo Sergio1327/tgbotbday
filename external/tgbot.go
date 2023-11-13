f7726e34319fc7476e9e3b6812d399ea72b2724c
An Sergey <an.sergey@sarkor.uz>
2023-11-13 17:03:23 +0500

    (test):

--- external/tgbot.go
+++ external/tgbot.go
@@ -31,7 +31,7 @@ func (e *BOT) RunBOT(sm transaction.SessionManager) {
 
 	for {
 		currentTime := time.Now()
-		if currentTime.Weekday() == time.Monday && currentTime.Hour() == 16 && currentTime.Minute() == 50 {
+		if currentTime.Weekday() == time.Monday && currentTime.Hour() == 17 && currentTime.Minute() == 5 {
 			fmt.Println("tets")
 			e.Usecase.BDay.LoadBDays(currentTime)
 			time.Sleep(time.Minute)
